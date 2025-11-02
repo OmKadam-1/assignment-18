02c0f24bd3e3361b44c8b3799749bdec2a815a4d
OmKadam-1 <kom110798@gmail.com>
2025-11-02 21:58:05 +0530

    correction

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Home from "./home";
+import Home from "./Home.jsx";
 import Contact from "./contact";
 import About from "./about";
 import Navbar from "./Navbar.jsx";
@@ -8,11 +8,11 @@ const root = createRoot(document.getElementById('root'));
 
 const path = window.location.pathname;
 console.log("Current path:", path);
-if (path == '/') {
-  root.render(<Home />)
-} else if (path == '/about') {
+if (path === '/') {
+  root.render(<Home/>)
+} else if (path === '/about') {
   root.render(<About />)
-} else if (path == '/contact') {
+} else if (path === '/contact') {
   root.render(<Contact />)
 } else {
   root.render(<h1>404-Page Not Found</h1>)
