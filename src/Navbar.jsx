fbde2bd01bad02e9c8f228b6e767df554cd47400
OmKadam-1 <kom110798@gmail.com>
2025-11-02 22:14:19 +0530

    changes

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -4,8 +4,8 @@ function Navbar({ activeMenu }) {
     return (
         <div className="navbar">
             <a href="/" className={`menu `}>Home</a>
-            <a href="/about" className={`menu `}>About</a>
-            <a href="/contact" className={`menu `} >Contact</a>
+            <a href="./about" className={`menu `}>About</a>
+            <a href="./contact" className={`menu `} >Contact</a>
         </div>
     );
 }
