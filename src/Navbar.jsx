9399ccf4e37a286b8a77050b4539ee08e772aafc
OmKadam-1 <kom110798@gmail.com>
2025-11-02 22:28:02 +0530

    correction

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,12 +1,14 @@
 
 import "./Navbar.css";
-function Navbar() {
-    return (
-        <div className="navbar">
-            <a href="/" className={`menu `}>Home</a>
-            <a href="/about" className={`menu `}>About</a>
-            <a href="/contact" className={`menu `} >Contact</a>
-        </div>
-    );
+
+function Navbar({activeMenu}) {
+  return (
+    <div className="navbar">
+        <a href="/" className={`menu ${activeMenu === "/" ? "menu-active" : ""}`}>Home</a>
+        <a href="/about" className={`menu ${activeMenu === "/about" ? "menu-active" : ""}` }>About</a>
+        <a href="/contact" className={`menu ${activeMenu === "/contact" ? "menu-active" : ""}`}>Contact</a>
+    </div>
+  )
 }
+
 export default Navbar;
