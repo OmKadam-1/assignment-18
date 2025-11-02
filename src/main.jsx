9d1c2ac87d9f0e38db141bb881a35a6cafcc1023
OmKadam-1 <kom110798@gmail.com>
2025-11-02 21:49:58 +0530

    correction

--- src/main.jsx
+++ src/main.jsx
@@ -10,9 +10,9 @@ const path = window.location.pathname;
 console.log("Current path:", path);
 if (path == '/') {
   root.render(<Home />)
-} else if (path == '/About') {
+} else if (path == '/about') {
   root.render(<About />)
-} else if (path == '/Contact') {
+} else if (path == '/contact') {
   root.render(<Contact />)
 } else {
   root.render(<h1>404-Page Not Found</h1>)
